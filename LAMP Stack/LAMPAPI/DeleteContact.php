134c3cdf5e36ca54244c37b7c899b3c3f6388f11
jilbenbrito <125512468+jilbenbrito@users.noreply.github.com>
2025-01-29 02:28:14 -0500

    Add files via upload
    Added Project Files

---
+++ LAMP Stack/LAMPAPI/DeleteContact.php
@@ -0,0 +1,45 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    $id = $inData["id"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $stmt = $conn->prepare("DELETE from Contacts WHERE ID = ?");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            returnWithError("");
+        } else {
+            returnWithError("No contact found with that ID.");
+        }
+        
+        $stmt->close();
+        $conn->close();
+	}
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
